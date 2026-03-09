d68c0ef91bc9bc58a3317a61f112b27c0d7b8b83
CarlosRuiz2025 <andresruizmiranda3000@gmail.com>
2026-03-08 22:10:57 -0500

    Avance Sabado

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
 using VeterinariaWeb.Models;
 
@@ -6,7 +7,7 @@ namespace VeterinariaWeb.Controllers
 {
     public class ProductosController : Controller
     {
-        private readonly string cadenaConexion = "Server=localhost;database=veterinaria;User=sa; password=sqladmin;TrustServerCertificate=true";
+        private readonly string cadenaConexion = "Server=(localdb)\\AndresRuiz;Database=VETERINARIA;User Id=AndresRuiz;Password=12345;TrustServerCertificate=true";
 
         public IActionResult Index()
         {
@@ -17,7 +18,22 @@ namespace VeterinariaWeb.Controllers
         public IActionResult Detail(int id)
         {
             var productoBuscado = obtenerProductoPorId(id);
-            return View(productoBuscado); 
+            return View(productoBuscado);
+        }
+
+        public IActionResult Create()
+        {
+            var categorias = obtenerCategorias();
+            ViewBag.Categorias = new SelectList(categorias, "ID", "Nombre");
+            return View(new Producto());
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Producto producto)
+        {
+            var exito = CrearProducto(producto);
+            return RedirectToAction("Index");
         }
 
         #region . Private methods .
@@ -27,26 +43,21 @@ namespace VeterinariaWeb.Controllers
             var listaProductos = new List<Producto>();
             using (var conexion = new SqlConnection(cadenaConexion))
             {
-                using(var comando = new SqlCommand("SELECT * FROM Productos", conexion))
+                using (var comando = new SqlCommand("SELECT P.*, CP.Nombre AS NombreCategoria FROM Productos P INNER JOIN CategoriaProductos CP ON P.CategoriaID = CP.ID", conexion))
                 {
                     conexion.Open();
                     using (var lector = comando.ExecuteReader())
                     {
-                        if(lector!= null && lector.HasRows)
+                        if (lector != null && lector.HasRows)
                         {
                             while (lector.Read())
                             {
-                                listaProductos.Add(new Producto
-                                {
-                                    ID = lector.GetInt32(0),
-                                    Nombre = lector.GetString(1),
-                                    Descripcion = lector.GetString(2)
-                                });
+                                listaProductos.Add(convertirReaderEnProducto(lector));
                             }
                         }
                     }
                 }
-            }            
+            }
             return listaProductos;
         }
 
@@ -55,7 +66,7 @@ namespace VeterinariaWeb.Controllers
             var producto = new Producto();
             using (var conexion = new SqlConnection(cadenaConexion))
             {
-                using (var comando = new SqlCommand("SELECT * FROM Productos where ID = @ID", conexion))
+                using (var comando = new SqlCommand("SELECT P.*, CP.Nombre AS NombreCategoria FROM Productos P INNER JOIN CategoriaProductos CP ON P.CategoriaID = CP.ID WHERE P.ID = @ID", conexion))
                 {
                     comando.Parameters.AddWithValue("@ID", id);
                     conexion.Open();
@@ -64,18 +75,81 @@ namespace VeterinariaWeb.Controllers
                         if (lector != null && lector.HasRows)
                         {
                             lector.Read();
-                            producto = new Producto()
-                            {
-                                ID = lector.GetInt32(0),
-                                Nombre = lector.GetString(1),
-                                Descripcion = lector.GetString(2)
-
-                            };                           
+                            producto = convertirReaderEnProducto(lector);
                         }
                     }
                 }
             }
+
             return producto;
+        }
+
+        private List<Categoria> obtenerCategorias()
+        {
+            var listaCategorias = new List<Categoria>();
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                using (var comando = new SqlCommand("SELECT * FROM CategoriaProductos", conexion))
+                {
+                    conexion.Open();
+                    using (var reader = comando.ExecuteReader())
+                    {
+                        if (reader != null && reader.HasRows)
+                        {
+                            while (reader.Read())
+                                listaCategorias.Add(convertirReaderEnCategoria(reader));
+                        }
+                    }
+                }
+            }
+            return listaCategorias;
+        }
+
+        private bool CrearProducto(Producto producto)
+        {
+            var exito = false;
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                using (var comando = new SqlCommand("INSERT INTO Productos(Nombre, Descripcion, CategoriaID, Precio, Activo) " +
+                    "VALUES(@nombre,@descripcion,@categoria,@precio,'1')", conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", producto.Nombre);
+                    comando.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                    comando.Parameters.AddWithValue("@categoria", producto.CategoriaID);
+                    comando.Parameters.AddWithValue("@precio", producto.Precio);
+                    conexion.Open();
+                    exito = comando.ExecuteNonQuery() > 0;
+                }
+            }
+            return exito;
+        }
+
+        private Producto convertirReaderEnProducto(SqlDataReader lector)
+        {
+            return new Producto()
+            {
+                ID = lector.GetInt32(0),
+                Nombre = lector.GetString(1),
+                Descripcion = lector.GetString(2),
+                Imagen = lector.GetString(3),
+                Precio = lector.GetDecimal(4),
+                CategoriaID = lector.GetInt32(5),
+                Categoria = new Categoria()
+                {
+                    ID = lector.GetInt32(5),
+                    Nombre = lector.GetString(7)
+                }
+            };
+        }
+
+        private Categoria convertirReaderEnCategoria(SqlDataReader lector)
+        {
+            return new Categoria
+            {
+                ID = lector.GetInt32(0),
+                Nombre = lector.GetString(1),
+                Activo = lector.GetString(2)
+            };
         }
 
         #endregion
